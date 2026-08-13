2cebbb6e7bf9c94af1c69e876fee35a1f78e1446
wellingtongermanosantos-sudo <wellington.germano.santos@escola.pr.gov.br>
2026-08-13 08:46:24 -0300

    inicio

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const botao = document.getElementById('botao_tema');
+
+function alternarModo () {
+  document.body.classList.toggle('modo_escuro');
+
+  if(document.body.classList.contains('modo_escuro')) {
+    botao.textContent = 'Modo Claro';
+  }else {
+    botao.textContent = 'Modo Escuro';
+  }
+ 
+}
+botao.addEventListener('click', alternarModo);
 
